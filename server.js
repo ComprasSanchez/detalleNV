b51f1bfb1e7616cd46e85e71e29f95c368e12460
ManiaciNahuel <nahuelmaniaci@gmail.com>
2025-09-25 15:58:04 -0300

    PORT

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require("path");
 dotenv.config();
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
